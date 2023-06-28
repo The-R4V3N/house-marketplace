8fb5f3b8c901dde25a3626da60d40c417f28c0a4
Oliver Joisten <o.joisten@live.se>
2023-06-28 19:26:19 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
 // import EditListing from './pages/EditListing'
-// import Listing from './pages/Listing'
+import Listing from './pages/Listing'
 // import Contact from './pages/Contact'
 
 function App() {
@@ -30,12 +30,12 @@ function App() {
 			<Route path='/sign-up' element={<SignUp />} />
 			<Route path='/forgot-password' element={<ForgotPassword />} />
 			<Route path='/create-listing' element={<CreateListing />} />
-			{/* <Route path='/edit-listing/:listingId' element={<EditListing />} />
+			{/* <Route path='/edit-listing/:listingId' element={<EditListing />} /> */}
 			<Route
 			  path='/category/:categoryName/:listingId'
 			  element={<Listing />}
 			/>
-			<Route path='/contact/:landlordId' element={<Contact />} /> */}
+			{/* <Route path='/contact/:landlordId' element={<Contact />} /> */}
 		  </Routes>
 		  <Navbar />
 		</Router>
