e11c43f5ee3224c3b99dd6f62ad7486c69648b80
Oliver Joisten <o.joisten@live.se>
2023-06-29 00:22:31 +0200

    🐛 fix(Slider.jsx): fix missing import for Swiper modules and add missing style for Swiper component ✨ feat(Slider.jsx): add navigation to Swiper component and set height style The missing import for Swiper modules has been added to ensure proper functionality. Additionally, a missing style for the Swiper component has been added to set its height to 300px. Navigation has also been enabled for the Swiper component to allow users to navigate through the slides.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,71 +9,78 @@ import Spinner from './Spinner'
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Slider() {
-    const [loading, setLoading] = useState(true)
-    const [listings, setListings] = useState(null)
-  
-    const navigate = useNavigate()
-  
-    useEffect(() => {
-      const fetchListings = async () => {
-        const listingsRef = collection(db, 'listings')
-        const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
-        const querySnap = await getDocs(q)
-  
-        let listings = []
-  
-        querySnap.forEach((doc) => {
-          return listings.push({
-            id: doc.id,
-            data: doc.data(),
-          })
-        })
-  
-        setListings(listings)
-        setLoading(false)
-      }
-  
-      fetchListings()
-    }, [])
-  
-    if (loading) {
-      return <Spinner />
-    }
-  
-    if (listings.length === 0) {
-      return <></>
-    }
-  
-    return (
-      listings && (
-        <>
-          <p className='exploreHeading'>Recommended</p>
-  
-          <Swiper slidesPerView={1} pagination={{ clickable: true }}>
-            {listings.map(({ data, id }) => (
-              <SwiperSlide
-                key={id}
-                onClick={() => navigate(`/category/${data.type}/${id}`)}
-              >
-                <div
-                  style={{
-                    background: `url(${data.imageUrls[0]}) center no-repeat`,
-                    backgroundSize: 'cover',
-                  }}
-                  className='swiperSlideDiv'
-                >
-                  <p className='swiperSlideText'>{data.name}</p>
-                  <p className='swiperSlidePrice'>
-                    ${data.discountedPrice ?? data.regularPrice}{' '}
-                    {data.type === 'rent' && '/ month'}
-                  </p>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </>
-      )
-    )
-  }
-  
-  export default Slider
+	const [loading, setLoading] = useState(true)
+	const [listings, setListings] = useState(null)
+
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		const fetchListings = async () => {
+			const listingsRef = collection(db, 'listings')
+			const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
+			const querySnap = await getDocs(q)
+
+			let listings = []
+
+			querySnap.forEach((doc) => {
+				return listings.push({
+					id: doc.id,
+					data: doc.data(),
+				})
+			})
+
+			setListings(listings)
+			setLoading(false)
+		}
+
+		fetchListings()
+	}, [])
+
+	if (loading) {
+		return <Spinner />
+	}
+
+	if (listings.length === 0) {
+		return <></>
+	}
+
+	return (
+		listings && (
+			<>
+				<p className='exploreHeading'>Recommended</p>
+				<Swiper
+					modules={[Navigation, Pagination, Scrollbar, A11y]}
+					slidesPerView={1}
+					pagination={{ clickable: true }}
+					navigation
+					style={{ height: '300px' }}
+				>
+					{listings.map(({ data, id }) => {
+						return (
+							<SwiperSlide
+								key={id}
+								onClick={() => navigate(`/category/${data.type}/${id}`)}
+							>
+								<div
+									className='swiperSlideDiv'
+									style={{
+										background: `url(${data.imageUrls[0]}) center no-repeat`,
+										backgroundSize: 'cover',
+									}}
+								>
+									<p className='swiperSlideText'>{data.name}</p>
+									<p className='swiperSlidePrice'>
+										${data.discountedPrice ?? data.regularPrice}{' '}
+										{data.type === 'rent' && '/ month'}{' '}
+									</p>
+								</div>
+							</SwiperSlide>
+						)
+					})}
+				</Swiper>
+			</>
+		)
+	)
+}
+
+export default Slider
